ec61f5bca19dae15433cdd48529d427078159be9
rockstarcuba <102777282+rockstarcuba@users.noreply.github.com>
2022-04-03 12:07:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -240,7 +240,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'obisoftdev'
+        tl_admin_user = 'rockstar984'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -558,7 +558,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5270983050:AAF9-0hePS70oBWP4bmCZaQZUXfOvyhI1hA'
+    bot_token = '5132478212:AAEkCET9Fav-jad83rYcE1ASVERTU-nuxZA'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
