2253b804df52a1bdd66adafe91f6d35ee12ac5b2
yahooymn <103601786+yahooymn@users.noreply.github.com>
2022-04-13 14:59:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -240,7 +240,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'rockstar984'
+        tl_admin_user = 'FrikiDota'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -542,7 +542,7 @@ def onmessage(update,bot:ObigramClient):
                 # set in debug
             #    api_id = 7386053
             #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-            #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+            #    bot_token = '5350050707:AAEVUKY8xHuPY0_mskSNgE6dySxm4AiHWVU'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
@@ -558,7 +558,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5267170951:AAEqx23n7KRgx_-qey41Fhw_VnxWf2IG9iU'
+    bot_token = '5350050707:AAEVUKY8xHuPY0_mskSNgE6dySxm4AiHWVU'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
